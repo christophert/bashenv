a22ec8dfeb19fb5591ca94447bc948dcf99d23aa
Christopher Tran! <enterprise-u@devices.christran.in>
2014-09-16 09:34:35 -0400

    changed to real files

--- .bash_aliases
+++ .bash_aliases
@@ -1,1 +1,5 @@
-/home/commander/.bash_aliases
+#bash aliases
+alias fuck='sudo $(history -p \!\!)'
+alias cspy='python3 $CSPR'
+alias cspd='cd ~/Documents/Projects/csci141/'
+alias cs='cspd && . ~/bin/csinit.sh $1'
--- bin/csinit.sh
+++ bin/csinit.sh
@@ -1,1 +1,9 @@
-/home/commander/bin/csinit.sh
+#!/bin/bash
+if [ -z $1 ]; then
+	echo "Usage: csinit.sh <program name>"
+	return
+else
+	echo "exporting program path to $1.py"
+	export CSPR=$1.py
+	echo "Done, use cspy to run program."
+fi
