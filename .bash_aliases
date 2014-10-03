e197d2289f58da265e7b834e08aa3949c23e66ad
commander@enterprise-u <enterprise-u-temporary@devices.christran.in>
2014-10-02 21:04:02 -0400

    updated aliases and implemented subject check for csinit

--- .bash_aliases
+++ .bash_aliases
@@ -7,7 +7,7 @@ alias fuck='sudo $(history -p \!\!)'
 alias cspy='python3 $CSPR $1'
 alias cspwd='cd ~/Documents/Projects/csci141/'
 alias prpwd='cd ~/Documents/Projects/'
-alias cs='cspwd && . ~/bin/csinit.sh $1'
+alias cs='cspwd && . ~/bin/csinit.sh $1 $2'
 
 #git aliases
 alias ctclone='prpwd && ~/bin/ctclone.sh $1' 
--- bin/csinit.sh
+++ bin/csinit.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-if [ -z $1 ]; then
-	echo "Usage: csinit.sh <program name>"
+#define function for documentation stuff
+doc ()
+{
+	echo "Usage: csinit.sh <type> <program name>"
+        echo "Type: hw/labs"
+}
+
+if [ -z $1 -a -z $2 ]; then
+	doc
+	return
+elif [ ! -d "$1" ]; then
+	doc
 	return
 else
-	echo "exporting program path to $1.py"
-	export CSPR=$1.py
+	echo "exporting program path to $1/$2.py"
+	export CSPR=$1/$2.py
 	echo "Done, use cspy to run program."
 fi
