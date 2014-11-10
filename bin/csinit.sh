7a517ec9be59de7e992cb58aca3472ba7554311e
commander <enterprise-u@devices.christran.in>
2014-11-10 18:06:36 -0500

    sort by directories on script and check for rit_object

--- bin/csinit.sh
+++ bin/csinit.sh
@@ -12,15 +12,22 @@ if [ -z $1 -a -z $2 ]; then
 elif [ ! -d "$1" ]; then
 	doc
 	return
+elif [ ! -d "$1/$2" ]; then
+	doc
+	return
 else
-	if [ ! -f "$1/$2.py" ]; then
+	if [ ! -f "$1/$2/$2.py" ]; then
 		echo "=================================================="
 		echo "!! WARNING !!"
 		echo "Program does not currently exist in the directory."
 		echo "It may need to be created!"
 		echo "=================================================="
 	fi
-	echo "exporting program path to $1/$2.py"
-	export CSPR=$1/$2.py
+	echo "exporting program path to $1/$2/$2.py"
+	export CSPR=$1/$2/$2.py
+	if [ ! -f "$1/$2/rit_object.py" ]; then
+		echo "copying rit_object to folder..."
+		cp $1/rit_object.py $1/$2/
+	fi
 	echo "Done, use cspy to run program."
 fi
