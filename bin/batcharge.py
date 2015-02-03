7317cd53db60f066eb46be8e2a8c305cecd5eaf4
officer <officer@enterprise.devices.christran.in>
2015-02-03 10:15:13 -0500

    add vim configuration

--- bin/batcharge.py
+++ bin/batcharge.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python
-# saved to ~/bin/batcharge.py and from
-# http://stevelosh.com/blog/2010/02/my-extravagant-zsh-prompt/#my-right-prompt-battery-capacity
-#!/usr/bin/env python
 # coding=UTF-8
 
 import math, subprocess
@@ -21,17 +18,16 @@ charge_threshold = int(math.ceil(10 * charge))
 # Output
 
 total_slots, slots = 10, []
-filled = int(math.ceil(charge_threshold * (total_slots / 10.0))) * u'◼'
-# old arrow: ▹▸▶
-empty = (total_slots - len(filled)) * u'◻'
+filled = int(math.ceil(charge_threshold * (total_slots / 10.0))) * u'▸'
+empty = (total_slots - len(filled)) * u'▹'
 
 out = (filled + empty).encode('utf-8')
 import sys
 
-color_green = '%{[32m%}'
-color_yellow = '%{[33m%}'
-color_red = '%{[31m%}'
-color_reset = '%{[00m%}'
+color_green = '%{[32m%}'
+color_yellow = '%{[1;33m%}'
+color_red = '%{[31m%}'
+color_reset = '%{[00m%}'
 color_out = (
     color_green if len(filled) > 6
     else color_yellow if len(filled) > 4
@@ -40,5 +36,3 @@ color_out = (
 
 out = color_out + out + color_reset
 sys.stdout.write(out)
-
-
